a44fffbca4e46713b382c26f69faf44d6d9f4536
JustEvil <thatjustevil@gmail.com>
2024-05-14 21:26:41 -0600

    I should check this out another day

--- src/generator/renderers/reply.tsx
+++ src/generator/renderers/reply.tsx
@@ -5,32 +5,28 @@ import MessageContent, { RenderType } from './content';
 import type { Message } from 'seyfert';
 import { UserFlags } from 'seyfert/lib/types';
 
-export default async function MessageReply({ message, context }: { message: Message; context: RenderMessageContext }) {
-  const guild = await message.guild();
-  
+export default async function MessageReply({ message, context }: { message: Message; context: RenderMessageContext }) {  
   if (!message.messageReference) return null;
   if (message.messageReference.channelId !== message.channelId) return null;
 
-  const referencedMessage = context.messages.find((m) => m.id === message.messageReference!.messageId);
+  const referencedMessage = context.messages.find((m) => m.id === message.referencedMessage?.id);
   if (!referencedMessage) return <DiscordReply slot="reply">Message could not be loaded.</DiscordReply>;
-  
-  const channel = await message.channel();
 
-  const isCrosspost = referencedMessage.messageReference && referencedMessage.messageReference.guildId !== guild?.id;
+  const channel = await message.channel();
   const isCommand = !!referencedMessage.interaction;
-  
+
   return (
     <DiscordReply
       slot="reply"
       edited={!isCommand && referencedMessage.editedTimestamp !== null}
       attachment={referencedMessage.attachments.length > 0}
-      author={referencedMessage.author.tag}
-      avatar={referencedMessage.author.avatarURL({ size: 32 }) ?? undefined}
-      roleColor={`#${referencedMessage.author.accentColor?.toString(16).padStart(6, '0')}`}
-      bot={!isCrosspost && referencedMessage.author.bot}
+      author={message.author.tag}
+      avatar={message.author.avatarURL({ size: 32 })}
+      roleColor={`#${message.author.accentColor?.toString(16).padStart(6, '0')}`}
+      bot={message.author.bot}
       verified={referencedMessage.author.flags && (referencedMessage.author.flags & UserFlags.VerifiedBot) === UserFlags.VerifiedBot}
       op={channel.isThread() && referencedMessage.author.id === channel.ownerId}
-      server={isCrosspost ?? undefined}
+      server={false}
       command={isCommand}
     >
       {referencedMessage.content ? (
